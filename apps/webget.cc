289243568b4197a84562f44629b6aee41eaf372e
polaris <2608726255@qq.com>
2024-04-05 23:03:44 +0800

    finish check0

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,19 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket sock {};
+   sock.connect(Address(host, "http"));
+   string data_send = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\n" + "Connection: close\r\n\r\n";
+   sock.write(data_send);
+   std::string buf;
+   while (!sock.eof()) {
+       sock.read(buf);
+       std::cout << buf;
+       buf.clear();
+   }
+   sock.close();
 }
 
 int main( int argc, char* argv[] )
